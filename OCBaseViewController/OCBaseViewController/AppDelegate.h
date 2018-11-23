c0913e6eb75c8fa7c0665a5f528218861e018a80
Soul <SoulSpecialReal@gmail.com>
2018-11-23 14:47:23 +0800

    项目 - init

---
+++ OCBaseViewController/OCBaseViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCBaseViewController
+//
+//  Created by Soul on 2018/11/23.
+//  Copyright © 2018 Brooks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCBaseViewController/OCBaseViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCBaseViewController
+//
+//  Created by Soul on 2018/11/23.
+//  Copyright © 2018 Brooks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
